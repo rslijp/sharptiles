cea0b10af9847b7d7a5409c7a5737049ccdea2c9
Renzo Slijp <renzo.slijp@softcause.nl>
2017-03-22 16:40:51 +0100

    Added new functions for handling paths

---
+++ SharpTiles/org.SharpTiles.Expressions/Functions/PathCombineFunction.cs
@@ -0,0 +1,60 @@
+/*
+ * SharpTiles, R.Z. Slijp(2008), www.sharptiles.org
+ *
+ * This file is part of SharpTiles.
+ * 
+ * SharpTiles is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SharpTiles is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SharpTiles.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ using System;
+ using System.IO;
+
+namespace org.SharpTiles.Expressions.Functions
+{
+    public class PathCombineFunction : IFunctionDefinition
+    {
+        private static readonly FunctionArgument[] ARGUMENTS = new[]
+                             {
+                                 new FunctionArgument{ Type = typeof (string), Name = "root"},
+                                 new FunctionArgument{ Type = typeof (string), Name = "relative"}
+                             };
+
+        #region IFunctionDefinition Members
+
+        public string Name
+        {
+            get { return "pathCombine"; }
+        }
+
+        public FunctionArgument[] Arguments
+        {
+            get { return ARGUMENTS; }
+        }
+
+        public Type ReturnType
+        {
+            get { return typeof (string); }
+        }
+
+        public object Evaluate(params object[] parameters)
+        {
+            string root = (string) parameters[0] ?? "";
+            string relative = (string)parameters[1] ?? "";
+            var path = Path.Combine(root, relative);
+            string fullPath = Path.GetFullPath(path);
+            return fullPath.Replace("\\", "/");
+        }
+
+        #endregion
+    }
+}
