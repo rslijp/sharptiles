c9117ba54eda99770f40c1dea02268860ae459bf
Renzo Slijp <renzo.slijp@softcause.nl>
2017-04-12 11:08:37 +0200

    Missing files

---
+++ SharpTiles/org.SharpTiles.Templates/Processor/ITemplateProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using org.SharpTiles.Tags.Creators;
+
+namespace org.SharpTiles.Templates.Processor
+{
+    public interface ITemplateProcessor
+    {
+        void Process(ParsedTemplate template);
+    }
+}
---
+++ SharpTiles/org.SharpTiles.Templates/Processor/TemplateProcessorCollection.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using org.SharpTiles.Tags.Creators;
+using org.SharpTiles.Templates.Processor;
+
+namespace org.SharpTiles.Templates.Validators
+{
+    public class TemplateProcessorCollection : ITemplateProcessor
+    {
+        private readonly ITemplateProcessor[] _processors;
+
+        public TemplateProcessorCollection(params ITemplateProcessor[] processors)
+        {
+            _processors = processors ?? new ITemplateProcessor[0];
+        }
+
+        public void Process(ParsedTemplate template)
+        {
+            foreach (var processor in _processors)
+            {
+                processor?.Process(template);
+            }   
+        }
+
+        public override string ToString() => $"TemplateProcessorCollection[{string.Join(",",_processors.Select(v => v?.GetType().Name))}]";
+    }
+}
