fc34069cc13aa874159017149d4192af8a982a3e
henritersteeg <henri@ter-steeg.com>
2017-09-15 09:22:28 +0200

    Adding DateTime functions to FunctionLib

--- SharpTiles/org.SharpTiles.Expressions/Functions/BaseFunctionLib.cs
+++ SharpTiles/org.SharpTiles.Expressions/Functions/BaseFunctionLib.cs
@@ -47,6 +47,8 @@ namespace org.SharpTiles.Expressions.Functions
             RegisterFunction(new PathFunction());
             RegisterFunction(new PathCombineFunction());
             RegisterFunction(new FallbackFunction());
+            RegisterFunction(new AddDaysFunction());
+            RegisterFunction(new AddMonthsFunction());
         }
 
         public override string GroupName
