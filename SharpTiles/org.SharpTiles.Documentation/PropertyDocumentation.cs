ee25025459670091cf83c3deb851fade827fff35
Roland Zwaga <rbzwaga@gmail.com>
2018-05-31 13:49:17 +0200

    Added PropertyAnnotationAttribute Added DataType and Annotations properties to PropertyDocumentation Added some extra tests to PropertyDocumentationTest

--- SharpTiles/org.SharpTiles.Documentation/PropertyDocumentation.cs
+++ SharpTiles/org.SharpTiles.Documentation/PropertyDocumentation.cs
@@ -2,17 +2,17 @@
  * SharpTiles, R.Z. Slijp(2008), www.sharptiles.org
  *
  * This file is part of SharpTiles.
- * 
+ *
  * SharpTiles is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * SharpTiles is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with SharpTiles.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -26,6 +26,15 @@ using org.SharpTiles.Tags.DefaultPropertyValues;
 
 namespace org.SharpTiles.Documentation
 {
+    [DataContract]
+    public enum TagAttributeDataType
+    {
+        Text,
+        Number,
+        Boolean,
+        Enum
+    }
+
     [DataContract]
     public class PropertyDocumentation : IDescriptionElement
     {
@@ -45,6 +54,36 @@ namespace org.SharpTiles.Documentation
             DetermineDefault(property);
             _description = DescriptionAttribute.Harvest(property)??_messagePath.Description;
             _enumValues = GetEnumValues(_property.GetCustomAttribute<EnumProperyTypeAttribute>(false)?.EnumType);
+            DataType = DetermineDataType(_property);
+            Annotations = GetAnnotations(_property);
+        }
+
+        private string[] GetAnnotations(PropertyInfo property)
+        {
+            return property.GetCustomAttributes<PropertyAnnotationAttribute>().Select(a => a.Annotation).ToArray();
+        }
+
+        [DataMember]
+        public TagAttributeDataType DataType { get; }
+
+        [DataMember]
+        public string[] Annotations { get; }
+
+        private TagAttributeDataType DetermineDataType(PropertyInfo property)
+        {
+            if (property.GetCustomAttribute<EnumProperyTypeAttribute>() != null)
+            {
+                return TagAttributeDataType.Enum;
+            }
+            if (property.GetCustomAttribute<NumberPropertyTypeAttribute>() != null)
+            {
+                return TagAttributeDataType.Number;
+            }
+            if (property.GetCustomAttribute<BooleanPropertyTypeAttribute>() != null)
+            {
+                return TagAttributeDataType.Boolean;
+            }
+            return TagAttributeDataType.Text;
         }
 
         public PropertyDocumentation(ResourceKeyStack messagePath, PropertyAttribute property)
@@ -99,7 +138,7 @@ namespace org.SharpTiles.Documentation
 
         private void DetermineDefault(PropertyInfo property)
         {
-            var fallBack = (IDefaultPropertyValue)property.GetCustomAttributes(typeof(IDefaultPropertyValue), false).FirstOrDefault(); 
+            var fallBack = (IDefaultPropertyValue)property.GetCustomAttributes(typeof(IDefaultPropertyValue), false).FirstOrDefault();
             if(fallBack!=null)
             {
                 _default = fallBack.ToString();
@@ -112,7 +151,7 @@ namespace org.SharpTiles.Documentation
 
         private EnumValue[] GetEnumValues(Type enumType)
         {
-            
+
             return enumType != null ? Enum.GetValues(enumType)
                 .Cast<object>()
                 .Select(v => EnumValue.Create(enumType, v))
